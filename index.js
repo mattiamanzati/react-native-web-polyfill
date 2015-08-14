1cec29d1b1ec781930e744f8b2ab85ba281d7125
Mattia Manzati <mattia.manzati@gmail.com>
2015-08-14 10:42:26 +0200

    added a NotPortedYet alert for the components

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ React.View = require('./components/View');
 React.Text = require('./components/Text');
 React.Image = require('./components/Image');
 
+// Not ported yet.
+var notPortedYetComponents = ['ActivityIndicatorIOS', 'DatePickerIOS', 'ListView', 'MapView',
+  'Navigator', 'NavigatorIOS', 'PickerIOS', 'ScrollView', 'SegmentedControlIOS',
+  'SliderIOS', 'SwitchIOS', 'TabBarIOS', 'TextInput', 'TouchableHighlight', 'TouchableOpacity',
+  'TouchableWithoutFeedback', 'WebView'];
+for(var i = 0; i < notPortedYetComponents.length; i++){
+  React[notPortedYetComponents[i]] = require('./components/NotPortedYet');
+}
+
 // exports
 module.exports = React;
---
+++ src/components/NotPortedYet.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Radium = require('radium');
+var browserifyStyle = require('../utils/browserifyStyle');
+
+class NotPortedYet extends React.Component{
+    render(){
+        return <div className="view" style={browserifyStyle(this.props.style)}>
+          Sorry, this component has not been ported yet.
+        </div>;
+    }
+}
+
+module.exports = Radium(View);
