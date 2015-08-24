47d6167bf94de831651f081e5a4d545993227873
mattiamanzati <mattia.manzati@gmail.com>
2015-08-24 17:18:39 +0200

    added pointer events support

--- src/components/View.js
+++ src/components/View.js
@@ -4,11 +4,18 @@ var browserifyStyle = require('../utils/browserifyStyle');
 var fixOldFlexbox = require('../utils/fixOldFlexbox');
 
 class View extends React.Component{
-    render(){
-        return <div className="view" style={browserifyStyle(this.props.style)}>
-          {this.props.children}
-        </div>;
-    }
+  render(){
+    var classNames = ['view'];
+    var {pointerEvents} = this.props;
+
+    // if pointer events available, push them into the class
+    if(pointerEvents) classNames.push(pointerEvents);
+
+    // return the actual element
+    return <div className={classNames.join(' ')} style={browserifyStyle(this.props.style)}>
+      {this.props.children}
+    </div>;
+  }
 }
 
 module.exports = fixOldFlexbox(Radium(View));
